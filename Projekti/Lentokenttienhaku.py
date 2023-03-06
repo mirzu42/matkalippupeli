d0ea48def795b235f637d238362aefb4844e1796
LucasRJL <lucas.lamponen@edu.omnia.fi>
2023-03-06 11:02:37 +0200

    koodin siistimistä, sekä ilmansuuntien lisäys

--- Projekti/Lentokenttienhaku.py
+++ Projekti/Lentokenttienhaku.py
@@ -1,48 +1,45 @@
-import random
-
+import math
 from geopy.distance import distance
 import mysql.connector
 
 yhteys = mysql.connector.connect(
          host="127.0.0.1",
          port= 3306,
-         database="matkalippupeli",
+         database="lentopeli",
          user="root",
-         password="1234",
+         password="wombat",
          autocommit=True
          )
 
+
 def haeKaikkiKentat():
-    kentät = []
-    sql = "select airport.ident from airport inner join country on airport.iso_country = country.iso_country where country.name = 'Finland' and type != 'closed' and type !='heliport' and type !='small_airport';"
-    kursori = yhteys.cursor()
-    kursori.execute(sql)
-    tulos = kursori.fetchall()
-    if (kursori.rowcount>0):
-        for rivi in tulos:
-            kentät.append(rivi[0])
-    return kentät
+    sql = """SELECT ident, name, latitude_deg, longitude_deg
+     FROM airport
+     WHERE iso_country = 'FI' 
+     AND type in('medium_airport', 'large_airport')
+     """
+    cursor = yhteys.cursor(dictionary=True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+
+
 def haeSijainti(icaoKoodi):
-    lat, lon =(0,0)
-    sql = "select ident, latitude_deg, longitude_deg from airport where ident ='"+icaoKoodi+"'"
+    lat, lon = (0, 0)
+    sql = "select latitude_deg, longitude_deg from airport where ident ='"+icaoKoodi+"'"
     kursori = yhteys.cursor()
     kursori.execute(sql)
-    tulos = kursori.fetchall()
-    if (kursori.rowcount>0):
-        for rivi in tulos:
-            lat=rivi[1]
-            lon=rivi[2]
+    tulos = kursori.fetchone()
+    if tulos:
+        lat, lon = tulos
     return lat, lon
 
 
 def laskeValimatka(icaoEka, icaoToka):
-    latitudeOne = haeSijainti(icaoEka)[0]
-    longitudeOne = haeSijainti(icaoEka)[1]
-    latitudeTwo = haeSijainti(icaoToka)[0]
-    longitudeTwo = haeSijainti(icaoToka)[1]
-    a = latitudeOne, longitudeOne
-    b = latitudeTwo, longitudeTwo
+    a = haeSijainti(icaoEka)
+    b = haeSijainti(icaoToka)
     return (distance(a, b))
+
 
 def saavutettavatLentokentät(icao, a_ports, p_range):
     in_range = []
@@ -53,28 +50,16 @@ def saavutettavatLentokentät(icao, a_ports, p_range):
     return in_range
 
 
-
-
-'''def lipunLähtö():
-    lähtö = 'select name from airport where iso_country = "FI" and type in("medium_airport", "large_airport") order by rand() limit 1;'
-
-    cursor = yhteys.cursor(dictionary=True)
-    cursor.execute(lähtö)
-   # if lähtö == kohde:
-    #    kohde = 'select name from airport where iso_country = "FI" and type in("medium_airport", "large_airport") order by rand() limit 1;'
-    tulos = cursor.fetchall()
-    return tulos
-def lipunKohde():
-    kohde = 'select name from airport where iso_country = "FI" and type in("medium_airport", "large_airport") order by rand() limit 1;'
-
-    cursor = yhteys.cursor(dictionary=True)
-    cursor.execute(kohde)
-    tulos = cursor.fetchall()
-    return tulos
-'''
-
-
-
-
-
-kentät = haeKaikkiKentat()
+def ilmanSuunnat(current_aport, in_range):
+    current_lat, current_lon = haeSijainti(current_aport)
+    compass_brackets = ["N", "NE", "E", "SE", "S", "SW", "W", "NW"]
+    for a_port in in_range:
+        lat, lon = haeSijainti(a_port['ident'])
+        delta_lat = lat - current_lat
+        delta_lon = lon - current_lon
+        radians = math.atan2(delta_lon, delta_lat)
+        degrees = math.degrees(radians)
+        degrees_positive = (degrees + 360) % 360
+        compass_lookup = round(degrees_positive / 45) % 8
+        a_port['ilmansuunta'] = compass_brackets[compass_lookup]
+    return in_range
